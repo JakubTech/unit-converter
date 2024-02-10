782fb3c976d94aa61ac57fd66dff070978ddecd3
Jakub <jakubnizio06@gmail.com>
2024-02-10 09:12:05 +0100

    increasing application speed

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 const convertResult = document.querySelector("#convert-result")
 const convertBtn = document.querySelector("#convert-btn")
-let inputEl = document.querySelector("#input-el")
-let unitSelect = document.querySelector("#unit-select")
+const inputEl = document.querySelector("#input-el")
+const unitSelect = document.querySelector("#unit-select")
 
 convertBtn.addEventListener("click", function() {
-    let units = unitSelect.value
     if(inputEl.value === "") {
         convertResult.innerHTML = ""
     } else {
+        const units = unitSelect.value
+        let unitResult = inputEl.value
+        let resultOfConvert = ""
         if(units === "ft-m") {
-            const unitResult = inputEl.value * 0.3048
-            convertResult.innerHTML = `${inputEl.value} ft = ${unitResult.toFixed(4)} m`
-            inputEl.value = ""
+            unitResult *= 0.3048
+            resultOfConvert = `${inputEl.value} ft = ${unitResult.toFixed(4)} m`
         } else if (units === "yd-m"){
-            const unitResult = inputEl.value * 0.9144
-            convertResult.innerHTML = `${inputEl.value} yd = ${unitResult.toFixed(4)} m`
-            inputEl.value = ""
+            unitResult *= 0.9144
+            resultOfConvert = `${inputEl.value} yd = ${unitResult.toFixed(4)} m`
         } else if (units === "in-m"){
-            const unitResult = inputEl.value * 0.0254
-            convertResult.innerHTML = `${inputEl.value} in = ${unitResult.toFixed(4)} m`
-            inputEl.value = ""
+            unitResult *= 0.0254
+            resultOfConvert = `${inputEl.value} in = ${unitResult.toFixed(4)} m`
         } else if (units === "oz-g"){
-            const unitResult = inputEl.value * 28.3495231
-            convertResult.innerHTML = `${inputEl.value} oz = ${unitResult.toFixed(4)} g`
-            inputEl.value = ""
+            unitResult *= 28.3495231
+            resultOfConvert = `${inputEl.value} oz = ${unitResult.toFixed(4)} g`
         } else if (units === "lb-kg"){
-            const unitResult = inputEl.value * 0.45359237
-            convertResult.innerHTML = `${inputEl.value} lb = ${unitResult.toFixed(4)} kg`
-            inputEl.value = ""
+            unitResult *= 0.45359237
+            resultOfConvert = `${inputEl.value} lb = ${unitResult.toFixed(4)} kg`
         }  else if (units === "m/s-km/h"){
-            const unitResult = inputEl.value * 3.6
-            convertResult.innerHTML = `${inputEl.value} m/s = ${unitResult.toFixed(1)} km/h`
-            inputEl.value = ""
+            unitResult *= 3.6
+            resultOfConvert = `${inputEl.value} m/s = ${unitResult.toFixed(1)} km/h`
         } else if (units === "mph-km/h"){
-            const unitResult = inputEl.value * 1.609344
-            convertResult.innerHTML = `${inputEl.value} mph = ${unitResult.toFixed(4)} km/h`
-            inputEl.value = ""
+            unitResult *= 1.609344
+            resultOfConvert = `${inputEl.value} mph = ${unitResult.toFixed(4)} km/h`
         } else if (units === "kn-km/h"){
-            const unitResult = inputEl.value * 1.852
-            convertResult.innerHTML = `${inputEl.value} kn = ${unitResult.toFixed(3)} km/h`
-            inputEl.value = ""
+            unitResult *= 1.852
+            resultOfConvert = `${inputEl.value} kn = ${unitResult.toFixed(3)} km/h`
         }
+        inputEl.value = ""
+        convertResult.innerHTML = resultOfConvert
     }
 })
